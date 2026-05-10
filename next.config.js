1d254aca02bde427c2c73a38089ca552d9bf6815
valerarubeth1 <117779070+valerarubeth1@users.noreply.github.com>
2026-05-10 09:28:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      'pino-pretty': false,
+      '@react-native-async-storage/async-storage': false,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
